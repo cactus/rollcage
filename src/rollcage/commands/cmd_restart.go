c5e41da9ee4ba95fbede58627cd00307f22f9b34
elij <elij@wundrd.net>
2015-12-05 15:59:01 -0800

    add restart

---
+++ src/rollcage/commands/cmd_restart.go
@@ -0,0 +1,45 @@
+package commands
+
+import (
+	"fmt"
+
+	"rollcage/core"
+
+	"github.com/cactus/cobra"
+	"github.com/cactus/gologit"
+)
+
+func restartCmdRun(cmd *cobra.Command, args []string) {
+	// requires root
+	if !core.IsRoot() {
+		gologit.Fatalf("Must be root to stop\n")
+	}
+
+	jail, err := core.FindJail(args[0])
+	if err != nil {
+		gologit.Fatalf("No jail found by '%s'\n", args[0])
+	}
+
+	if !jail.IsRunning() {
+		gologit.Fatalf("Jail is not running!\n")
+	}
+
+	fmt.Printf("* Restarting %s (%s)\n", jail.HostUUID, jail.Tag)
+	core.CmdMust(
+		fmt.Errorf("Error restarting jail!"),
+		"/usr/sbin/jail", "-rc", fmt.Sprintf("ioc-%s", jail.HostUUID))
+}
+
+func init() {
+	RootCmd.AddCommand(&cobra.Command{
+		Use:   "restart UUID|TAG",
+		Short: "restart jail",
+		Long:  "Restart jail identified by UUID or TAG.",
+		Run:   restartCmdRun,
+		PreRun: func(cmd *cobra.Command, args []string) {
+			if len(args) == 0 {
+				gologit.Fatalln("Required UUID|TAG not provided")
+			}
+		},
+	})
+}
